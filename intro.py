79bab30812dc890250034b9fb2cb9b482d78fcfd
alanacalado <alanacalado1@gmail.com>
2024-07-25 09:49:43 -0300

    repositorio

---
+++ intro.py
@@ -0,0 +1,13 @@
+print("Hello World!")
+
+def desenhar_gato():
+    print(" /\\_/\\")
+    print("( o.o )")
+    print(" > ^ <")
+    print("( ( ) )")
+    print(" `==='")
+
+# Chamar a função para desenhar o gato gordinho
+desenhar_gato()
+
+
